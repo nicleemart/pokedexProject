bbacbc5daedd487a807392bffa60ba69b516e171
Nels <ncfindley2@gmail.com>
2016-07-13 09:09:57 -0500

    Write and read record methods added

--- app/modules/pokedex.rb
+++ app/modules/pokedex.rb
@@ -1,17 +1,33 @@
-	require 'csv'
 
-	new_pokemon = ["Josh", 28, 11, "Grass"]
+class Pokedex
 
-	CSV.open('pokedex.csv', 'w') do |csv|
-		csv << new_pokemon
+# Write record to pokedex.csv files
+	def Pokedex.pokedex_write_record(new_pokemon)
+		
+	
+		require 'csv'
+
+		new_pokemon = ["Josh", 28, 11, "Grass"]
+
+		CSV.open('pokedex.csv', 'w') do |csv|
+			csv << new_pokemon
+		end
 	end
 
 
+#Read all record in pokedex.csv and put into file.
+	def Pokedex.pokedex_all_records()
+		require 'csv'
+		list = Array.new
+		file = 'pokedex.csv'
 
-	list = Array.new
-		@file = pokedex.csv
+		CSV.foreach(file) do |record|
 
-		CSV.foreach(@file) do |@record|
-
-			list << @record
+			list << record
+			p list
+			return list
+		end
 	end
+
+
+end
