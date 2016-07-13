0cea9aa3a74e98d7df8ac885d6f42896ab20102d
Joshua Salyer <joshsalyer23@gmail.com>
2016-07-12 21:54:55 -0500

    Added save_pokemon function with comments

--- app/modules/pokedex.rb
+++ app/modules/pokedex.rb
@@ -1,17 +1,11 @@
-	require 'csv'
+require 'csv'
 
-	new_pokemon = ["Josh", 28, 11, "Grass"]
-
-	CSV.open('pokedex.csv', 'w') do |csv|
+#This method saves a new Pokemon's information in a text file as an Array
+#new_pokemon = the Array of traits gathered from the user (via form)
+def save_pokemon(new_pokemon)
+	#Open the file the new data will be saved in
+	CSV.open("pokedex.csv", "a") do |csv|
+		#Add the Array to the file
 		csv << new_pokemon
 	end
-
-
-
-	list = Array.new
-		@file = pokedex.csv
-
-		CSV.foreach(@file) do |@record|
-
-			list << @record
-	end
+end
