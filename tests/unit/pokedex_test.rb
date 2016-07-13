6cc1f991b3bedee7eba2ff107533bc4080a088d8
Nels <ncfindley2@gmail.com>
2016-07-13 11:06:19 -0500

    working on 2nd test

--- tests/unit/pokedex_test.rb
+++ tests/unit/pokedex_test.rb
@@ -1,4 +1,4 @@
-require_relative '../test_helper'
+require 'test_helper'
 
 class PokedexTest < Minitest::Test
 
@@ -8,4 +8,16 @@ class PokedexTest < Minitest::Test
 		Pokedex.pokedex_save_record(new_pokemon)
 	end
 	
+
+	def test_find_record
+
+		new_pokemon = ["Charmander,5,2,male,fire,35,30,yes,Charmander,Charmeleon,Charizard", "Squirtle,3,4,female,water,40,32,no,Squirtle,Wartortle,Blastoise"]
+		name_pokemon = "Squirtle"
+		name_test = Pokedex.pokedex_find_record(name_pokemon,pokedex_array)
+
+
+		assert_equal(name_pokemon,name_test)
+	end
+	
+
 end
