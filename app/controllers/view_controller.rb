db56dc05ecfcee2273dccc983e3d671c2a9cf6da
Nels <ncfindley2@gmail.com>
2016-07-13 16:36:50 -0500

    working on add/view page

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -1,4 +1,30 @@
 MyApp.get "/view" do 
+
+	name = params[:name]
+	height = params[:height]
+	gender = params[:gender]
+	type = params[:type]
+	cp = params[:cp]
+	hp = params[:hp]
 	
+	puts name
+
+	if height != ""
+	
+	new_pokemonarray = []
+	new_pokemonarray << name
+	new_pokemonarray << height
+	new_pokemonarray << gender
+	new_pokemonarray << type
+	new_pokemonarray << cp
+	new_pokemonarray << hp
+
+	Pokedex.pokedex_save_record(new_pokemonarray)
+	end
+
+
+
+
+
 	erb :"pokedex/view"
 end
