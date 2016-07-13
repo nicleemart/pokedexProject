f24e784dacf739d1f725d49cf50b1ccd34034a70
Nels <ncfindley2@gmail.com>
2016-07-13 09:53:44 -0500

    Working on test file

---
+++ tests/unit/pokedex_test.rb
@@ -0,0 +1,9 @@
+require_relative '../test_helper'
+
+class PokedexTest < Minitest::Test
+	def test_add_record
+
+		new_pokemon = [Charmander,5,2,male,fire,35,30,yes,Charmander,Charmeleon,Charizard]
+		Pokedex.pokedex_save_record(new_pokemon)
+	end
+end
