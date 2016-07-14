bb489b2c3c26f07e3c9afa452e302a5f0930541c
Nels <ncfindley2@gmail.com>
2016-07-14 10:41:27 -0500

    view/add pages working

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -1,25 +1,28 @@
 MyApp.get "/view" do 
 
-	name = params[:name]
-	height = params[:height]
-	gender = params[:gender]
-	type = params[:type]
-	cp = params[:cp]
-	hp = params[:hp]
 	
-	puts name
-
-	if height != ""
+	@name = params[:name]
+	@height = params[:height]
+	@weight = params[:weight]
+	@gender = params[:gender]
+	@type = params[:type]
+	@cp = params[:cp]
+	@hp = params[:hp]
 	
-	new_pokemonarray = []
-	new_pokemonarray << name
-	new_pokemonarray << height
-	new_pokemonarray << gender
-	new_pokemonarray << type
-	new_pokemonarray << cp
-	new_pokemonarray << hp
+	
 
-	Pokedex.pokedex_save_record(new_pokemonarray)
+	if @height != ""
+	
+	@new_pokemonarray = []
+	@new_pokemonarray << @name
+	@new_pokemonarray << @height
+	@new_pokemonarray << @weight
+	@new_pokemonarray << @gender
+	@new_pokemonarray << @type
+	@new_pokemonarray << @cp
+	@new_pokemonarray << @hp
+
+	Pokedex.pokedex_save_record(@new_pokemonarray)
 	end
 
 
