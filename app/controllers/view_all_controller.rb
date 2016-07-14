56faeda3041539ee52be2fce52a80e2d6b3bb5a7
Joshua Salyer <joshsalyer23@gmail.com>
2016-07-13 23:05:34 -0500

    View all page shows all the pokemon in the Pokedex

--- app/controllers/view_all_controller.rb
+++ app/controllers/view_all_controller.rb
@@ -1,4 +1,10 @@
 MyApp.get "/viewall" do 
-	
+	@all_pokemon = Pokedex.pokedex_all_records
+	# if @all_pokemon.empty? == true
+	# 	puts "Visit the Add Pokemon page to start building your Pokedex"
+
+	# else
+	# 	@all_pokemon
+	# end
 	erb :"pokedex/view_all"
 end
