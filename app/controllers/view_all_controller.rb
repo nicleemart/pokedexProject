7fd04b37a8f958d151bc820e84bc93cd6a3abda6
Joshua Salyer <joshsalyer23@gmail.com>
2016-07-14 08:43:34 -0500

    Added error messages to home and view all pages

--- app/controllers/view_all_controller.rb
+++ app/controllers/view_all_controller.rb
@@ -1,5 +1,5 @@
 MyApp.get "/viewall" do 
 	@all_pokemon = Pokedex.pokedex_all_records
-
+	@no_pokemon_error = "Visit the Add Pokemon page to start building your Pokedex"
 	erb :"pokedex/view_all"
 end
