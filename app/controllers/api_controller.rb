72e1e7f98007e3c542995c98deb037985080f399
Nels <ncfindley2@gmail.com>
2016-07-22 09:02:53 -0500

    wp

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,13 @@
-# This is for getting information about a SINGLE Pokemon
-MyApp.get "/api/pokemon/:name" do
-	@pokemon_data = # Method that fills flat storage data into API data shell (Hash and Arrays)
+# # This is for getting information about a SINGLE Pokemon
+# MyApp.get "/api/pokemon/:name" do
+# 	@pokemon_data = # Method that fills flat storage data into API data shell (Hash and Arrays)
 
-	return @pokemon_data # somehow convert the data to JSON
-end
+# 	return @pokemon_data # somehow convert the data to JSON
+# end
 
-# This is for getting information about a ALL Pokemon
-MyApp.get "/api/all_pokemon" do
-	@pokemon_data = # Method that fills flat storage data into API data shell (Hash and Arrays)
+# # This is for getting information about a ALL Pokemon
+# MyApp.get "/api/all_pokemon" do
+# 	@pokemon_data = # Method that fills flat storage data into API data shell (Hash and Arrays)
 
-	return @pokemon_data # somehow convert the data to JSON
-end
+# 	return @pokemon_data # somehow convert the data to JSON
+# end
