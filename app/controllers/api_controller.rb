95ed7ec70b105043338db4b7cc0d40c01f571df2
Joshua Salyer <joshsalyer23@gmail.com>
2016-07-21 22:56:44 -0500

    JSON shows up but isn't valid for some reason

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,9 +5,9 @@
 # 	return @pokemon_data # somehow convert the data to JSON
 # end
 
-# # This is for getting information about a ALL Pokemon
-# MyApp.get "/api/all_pokemon" do
-# 	@pokemon_data = # Method that fills flat storage data into API data shell (Hash and Arrays)
-
-# 	return @pokemon_data # somehow convert the data to JSON
-# end
+# This is for getting information about a ALL Pokemon
+MyApp.get "/api/all_pokemon" do
+	@file = File.open('Data_File/api.json')
+	# @data_hash = JSON.parse(@file)
+	return @file
+end
