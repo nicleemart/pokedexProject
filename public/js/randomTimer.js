8c5b1a7cf16c4055c1ce447e4ed519205acbe1d0
Nikki <nicleemart@gmail.com>
2016-07-31 19:12:14 -0500

    wip

--- public/js/randomTimer.js
+++ public/js/randomTimer.js
@@ -1,5 +1,22 @@
 window.addEventListener("load", function(){
-	setTimeout(function(){
-    	location = ""
-  			},7000)
+
+	function refreshStats (){
+
+	var runFunction = new XMLHttpRequest();
+
+
+  	function runFunction (){
+    	var stat = document.getElementsByClassName("stats__info");
+    	for (i = 0; i < stat.length; i++){
+    		// stat[i].innerHTML = e.target.responseText;
+    	}
+    }
+
+		runFunction.open ("get", "http://localhost:9292", true);
+		runFunction.send();
+
+		setTimeout(refreshStats, 2000);
+
+	};
+
 });
